70ed56fbf454e9c92a58c91cbd7057b513b23d1b
David <dbonnaud123@gmail.com>
2022-12-07 02:19:11 -0500

    finished rest of functionality, generally all works, just needs ui changes

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,22 +1,47 @@
 import Stripe from "stripe";
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    //console.log(req.body);
     try {
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
+      const params = {
+        submit_type: 'pay',
         mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
-      res.redirect(303, session.url);
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_address_collection: {'allowed_countries': ['US']},
+        shipping_options: [
+          { shipping_rate: 'shr_1MC9joCULWfQ3WmAochQy7j7' },
+          { shipping_rate: 'shr_1MC9kMCULWfQ3WmARjlgHHIh' }
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/bci1lmzr/production/').replace('-jpg', '.jpg');
+
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      }
+
+      const session = await stripe.checkout.sessions.create(params);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
